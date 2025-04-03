ab5c626abea400601e2bd15ade703914dc8f1e97
Elijah Bustamante <124417882+choybuch@users.noreply.github.com>
2025-04-04 03:56:33 +0800

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -14,9 +14,8 @@ const Footer = () => {
         <div>
           <p className='text-xl font-medium mb-5'>COMPANY</p>
           <ul className='flex flex-col gap-2 text-gray-600'>
-            <li>Home</li>
-            <li>About us</li>
-            <li>Delivery</li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About us</Link></li>
             <li>Privacy policy</li>
           </ul>
         </div>
